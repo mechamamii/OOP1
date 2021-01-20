cff1aae6684a238c0c031346136e16166ef33e29
muhammed diri <muhmdaley@gmail.com>
2021-01-20 23:55:32 +0300

    Add project files.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    // tip "T"olarak yazdık
+    //item eleman demek
+    class MyList<T>
+    {
+        T[] items;
+        //constructor="kurucu"demek
+        public MyList()
+        {
+            items = new T[0];
+        }
+        //eleman item demek
+        //array dizi
+        public void Add(T item)
+        {
+            //geciciDizi tempArray
+            T[] tempArray = items;
+            //diziye eleman eklemek
+            items = new T[items.Length+1];//elemanlarıbir artırmak
+            for (int i = 0; i < tempArray.Length; i++) 
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
